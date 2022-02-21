6477d8905fdd23222685e5d8c4d407c5d97d85c9
Drumshtick <ekodnar@outlook.com>
2022-02-21 16:15:59 -0600

    initialized redux

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from "./reducers";
+
+const store = createStore(  
+  rootReducer,  
+  { itemActions: { favourites: [] } }, // sets initial state 
+  composeWithDevTools() // makes debugging through Redux Dev Tools possible
+  );
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<Provider store={store}>tApp </Provider>, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
