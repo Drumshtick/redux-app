79534cf2621afb0146b9c8be58575a79243093ab
Drumshtick <ekodnar@outlook.com>
2022-02-21 16:39:09 -0600

    altered spelling of state

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ import rootReducer from "./reducers";
 
 const store = createStore(  
   rootReducer,  
-  { itemActions: { favourites: [] } }, // sets initial state 
+  { itemActions: { favorites: [] } }, // sets initial state 
   composeWithDevTools() // makes debugging through Redux Dev Tools possible
   );
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(<Provider store={store}>tApp </Provider>, rootElement);
+ReactDOM.render(<Provider store={store}>App</Provider>, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals();
